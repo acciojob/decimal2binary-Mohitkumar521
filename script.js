8dc4dddf609006750c42517ed056b88b629da795
Mohit Kumar <109014039+Mohitkumar521@users.noreply.github.com>
2023-10-12 12:29:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimal) {
+    if (decimal < 0) {
+        return "Negative numbers are not supported.";
+    } else if (decimal === 0) {
+        return "0";
+    } else {
+        let binary = "";
+        while (decimal > 0) {
+            const remainder = decimal % 2;
+            binary = remainder + binary;
+            decimal = Math.floor(decimal / 2);
+        }
+        return binary;
+    }
 }
-
-window.decimalToBinary = decimalToBinary;
